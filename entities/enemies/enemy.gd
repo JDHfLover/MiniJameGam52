f28adc96f141eb8c741657f9f535a8112d9c9139
FlowerLoverJDH <rotsu1111@email.com>
2026-03-06 23:12:01 +0200

    Added angry glass

--- entities/enemies/enemy.gd
+++ entities/enemies/enemy.gd
@@ -6,12 +6,26 @@ class_name Enemy
 @export var damage: float
 @export var max_health:float 
 @onready var health := max_health 
+var is_moving := false
 
-@onready var path: Path2D
+@export var water_gradient: Gradient
+@onready var water: Sprite2D = $Water
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
 
+func _ready() -> void:
+	anim_player.play("spawn")
 
 func _physics_process(delta: float) -> void:
-	progress += speed * delta
+	water.modulate = water_gradient.sample(health/max_health)
+	if(is_moving): progress += speed * delta
+
+func _on_animation_finished(anim_name:String):
+	if (anim_name == "spawn"):
+		is_moving = true
+		anim_player.play("move")
+	if (anim_name == "die"):
+		queue_free()
 
 func take_damage(damage: float):
 	health-=damage
@@ -19,4 +33,5 @@ func take_damage(damage: float):
 	elif (health > max_health): health = max_health
 
 func die():
-	queue_free()
+	is_moving = false
+	anim_player.play("die")
